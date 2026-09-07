a50f63be7511fb608a0db343dbac9f6234c8a543
john unwin <jdunwin@gmail.com>
2026-09-07 13:59:41 -0500

    Add episodic memory contracts

---
+++ src/coala_memory/episodic/__init__.py
@@ -0,0 +1,9 @@
+from .interface import EpisodicMemory, EpisodicMemoryRequest, EpisodicMemoryResult, EpisodicEvent, EpisodicDigest
+
+__all__ = [
+    "EpisodicMemory",
+    "EpisodicMemoryRequest",
+    "EpisodicMemoryResult",
+    "EpisodicEvent",
+    "EpisodicDigest",
+]
