4117583b69fe46679b7c93515be8608f88747a78
john unwin <jdunwin@gmail.com>
2026-09-07 14:00:15 -0500

    Add procedural memory contracts

---
+++ src/coala_memory/procedural/__init__.py
@@ -0,0 +1,8 @@
+from .interface import ProceduralMemory, ProceduralMemoryRequest, ProceduralMemoryResult, ProceduralSkill
+
+__all__ = [
+    "ProceduralMemory",
+    "ProceduralMemoryRequest",
+    "ProceduralMemoryResult",
+    "ProceduralSkill",
+]
