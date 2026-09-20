f23c9c1d3ee26845be60aab6c81a49125e37c1f8
john unwin <jdunwin@gmail.com>
2026-09-20 11:52:55 -0500

    Import structured Galet handler in integration test

--- tests/test_galet_tools_integration.py
+++ tests/test_galet_tools_integration.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 from galet_tools import HandlerRegistry as GaletHandlerRegistry
 from galet_tools.framework import HandlerV2 as GaletHandlerV2
-from galet_tools.tools.command_execution_handler2 import (
-    CommandExecutionHandler2 as GaletCommandExecutionHandler2,
-)
+from galet_tools.tools.execute_command2 import ExecuteCommand2 as GaletExecuteCommand2
 from galet_tools.tools.file_load_handler2 import (
     FileLoadHandler2 as GaletFileLoadHandler2,
 )
